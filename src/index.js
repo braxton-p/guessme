7705a46e8bd6a62757f4fbc543d4c96100e31d31
Braxton Perry <50059391+braxton-p@users.noreply.github.com>
2021-12-07 20:06:54 -0500

    Add missing semicolon

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 // if nothing has used up the request until now
 // it must be a 404
 app.use((req, res, next) => {
-  res.status(404).send("404. That file must be on another server...")
+  res.status(404).send("404. That file must be on another server...");
 });
 
 app.listen(port, () => {
